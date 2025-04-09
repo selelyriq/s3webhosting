9bfa85ac0ee87380258643e2892c88a66d2c0f24
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-04-08 20:30:27 -0400

    cloudfront

--- main.tf
+++ main.tf
@@ -78,3 +78,37 @@ resource "aws_s3_object" "index" {
   source       = "${path.module}/index.html"
   content_type = "text/html"
 }
+
+resource "aws_cloudfront_distribution" "cloudfront_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.s3_bucket.id
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = aws_s3_bucket.s3_bucket.id
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
